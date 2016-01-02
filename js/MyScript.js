d46cfbfbf5cc63985247dffd2e3a8927dba168d5
quantsegu <shreshti@gmail.com>
2016-01-02 23:54:32 +0530

    Changed the product id

--- js/MyScript.js
+++ js/MyScript.js
@@ -3,7 +3,7 @@ var shoppingCartModule = angular.module("technologyCart", [])
 shoppingCartModule.controller("technologyController",
     function($scope) {
         $scope.items = [
-            {name: "Product 1", price: 50, rank:5},
+            {name: "Product one", price: 50, rank:5},
             {name: "Product 2", price: 20, rank: 3},
             {name: "Product 3", price: 180, rank: 4},
             {name: "Product 4", price: 180, rank: 4}
